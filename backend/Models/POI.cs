e520c29bfd7f564b9280a140158c5799b6d9914f
Ishay Sela <ishaisela@gmail.com>
2025-08-28 16:30:28 +0300

    added basic POI model

---
+++ backend/Models/POI.cs
@@ -0,0 +1,15 @@
+using System.Text;
+
+namespace GeoInformation.Models
+{
+    public record POI
+    {
+        public required string Id { get; init; }
+        public required string Name { get; init; }
+        public required string Description { get; init; }
+        public required string Category { get; init; }
+        public required double Latitude { get; init; }
+        public required double Longtitude { get; init; }
+        public required DateTime CreationTime { get; init; }
+    }
+}
--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e83101b9b78efdc66108ba8e67e05a834e19f18c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99a2af39c20bb5368362c27314cc633cc3cf039d")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
