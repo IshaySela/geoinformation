226427ec3faaadf4562a8294ad58e93e103a165f
Ishay Sela <ishaisela@gmail.com>
2025-09-01 00:01:05 +0300

    removed unused folder

--- frontend/src/components/Header.tsx
+++
@@ -1,6 +0,0 @@
-import { JSX } from "react";
-
-
-export function Header(): JSX.Element {
-    return <h1>This is the app header</h1>
-}
--- frontend/src/components/PoisMap.tsx
+++
@@ -1,7 +0,0 @@
-import { JSX } from "react";
-
-
-
-export function PoisMap(): JSX.Element {
-    return <h1>THis is the POIs map</h1>
-}
